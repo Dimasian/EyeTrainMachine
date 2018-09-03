25279dd19fc02ec5059981eaab0c9f5651c8384c
martid <martid1703@gmail.com>
2018-09-03 20:31:04 +0300

    ---Initial add to GitHub----

---
+++ Sensor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeTrainMachine_WinForms
+	{
+	public class Sensor
+		{
+		bool active { get; set; }
+		public Sensor ()
+			{
+
+			}
+		}
+	}
